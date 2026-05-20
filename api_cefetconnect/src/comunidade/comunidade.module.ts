8087fffde39d2530f41c8772be8ef8e640ec3e96
Rafaela Lorenzeto <rafaelaobraga@hotmail.com>
2026-05-20 10:43:32 -0300

    Adiciona importação do TypeOrmModule e entidades no ComunidadeModule para permitir acesso ao banco de dados
    - Importa TypeOrmModule do @nestjs/typeorm
    - Importa entidades Comunidade e Usuario
    - Registra Comunidade e Usuario no TypeOrmModule.forFeature para injeção de repositórios

--- api_cefetconnect/src/comunidade/comunidade.module.ts
+++ api_cefetconnect/src/comunidade/comunidade.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ComunidadeService } from './comunidade.service';
 import { ComunidadeController } from './comunidade.controller';
+import { Comunidade } from '../entities/comunidade.entity';
+import { Usuario } from '../entities/usuario.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Comunidade, Usuario])],
   controllers: [ComunidadeController],
   providers: [ComunidadeService],
 })
