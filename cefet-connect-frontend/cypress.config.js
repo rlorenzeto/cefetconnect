96633d07cd4a58848be204e5aceb039331aa58e0
johnatanduarte <johnatanduartefranco95@gmail.com>
2026-06-15 13:36:22 -0300

    mudanças na integração e novos testes e2e em produção

--- cefet-connect-frontend/cypress.config.js
+++ cefet-connect-frontend/cypress.config.js
@@ -4,8 +4,8 @@ module.exports = defineConfig({
   e2e: {
     // Força o Cypress a usar uma resolução de Desktop (ex: 1280x720)
     // Isso impede que o Tailwind esconda o formulário com a classe "hidden"
-    viewportWidth: 1280, 
-    viewportHeight: 720,  
+    viewportWidth: 1280,
+    viewportHeight: 720,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
